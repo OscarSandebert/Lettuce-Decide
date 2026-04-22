8f53f660187c08ab32a244dc746f2ae0fc6b7314
Steven Zhu <04stezhu@gmail.com>
2026-04-22 09:45:28 +0200

    0.9 Not complete

---
+++ Dabas.py
@@ -0,0 +1,24 @@
+"""
+Funkar inte än, väntar på svar från Dabas med API key
+"""
+
+
+import requests
+
+def getProduct(gtin):
+    url = f'https://api.dabas.com/DABASService/V2/article/gtin/{gtin}/json'
+    
+    headers = {
+        "Authorization": f'Bearer YOUR_API_KEY'
+    }
+    
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error ", response.status_code)
+        return None
+    
+product = getProduct("07310867502214")
+print(product)
