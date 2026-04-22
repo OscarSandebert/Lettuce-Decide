59d292a5761debebb334235f2f32bc7f27d1d7eb
Olle Knutsson <olleaxeljonas@gmail.com>
2026-04-22 11:45:26 +0200

    Funktion för att hämta miljödata

--- OpenFoodFacts.py
+++ OpenFoodFacts.py
@@ -1,28 +1,7 @@
 import requests
 import json
 
-def getProductEnvironmentalData(gtin):
-    # Vi använder v2-API:et för bättre stöd av miljödata
-    url = f'https://world.openfoodfacts.org/api/v2/product/{gtin}.json'
-
-    headers = {
-        # Använder din mail som User-Agent (bra praxis för OFF)
-        "User-Agent": "lettuce-decide (oscar.sandebert@gmail.com)"
-    }
-
-    try:
-        response = requests.get(url, headers=headers)
-        
-        if response.status_code != 200:
-            print(f"Fel vid anrop: {response.status_code}")
-            return None
-
-        data = response.json()
-        
-        if data.get("status") == 0 or "product" not in data:
-            print("Error: Produkten hittades inte")
-            return None
-
+def getProductEnvironmentalData(data):
         product = data["product"]
         
         eco_data = product.get("ecoscore_data", {})
@@ -35,10 +14,6 @@ def getProductEnvironmentalData(gtin):
         }
 
         return result
-
-    except Exception as e:
-        print(f"Ett oväntat fel uppstod: {e}")
-        return None
 
 # TESTA KODEN
 res = getProductEnvironmentalData("5000181028133")
